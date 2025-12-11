a24b9ec19c275f24644111888f38d57b1604daff
AshutoshRode <arode1999@gmail.com>
2025-12-11 19:06:28 +0530

    Google Gemini API Key setup is Done

---
+++ src/context/Context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
+
+
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+
+    const [input, setInput] = useState("")
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState([]);
+    const [showResult,setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData,setResultData] = useState("");
+
+    const onSent = async (prompt) => {
+        await runChat(prompt)
+    }
+
+
+
+    const ContextValue = {
+
+
+    }
+
+    return (
+        <Context.Provider value={ContextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+
+export default ContextProvider
