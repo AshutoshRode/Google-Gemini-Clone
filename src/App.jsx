62ac59f8d5080d2a8d10fe0fa46e03103c160e3f
AshutoshRode <arode1999@gmail.com>
2025-12-11 12:00:30 +0530

    Sidebar design is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Sidebar from "./components/Sidebar/Sidebar"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
 
+      <Sidebar />
 
     </>
   )
